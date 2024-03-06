53c2bd975ae9393424854607160ff80889e909fe
Michał Styliński <stylinskimichal@gmail.com>
2024-03-06 19:03:39 +0100

    improve app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Form } from './Form';
+import { currencies } from './currencies';
 
 function App() {
-  return (
-    <body className="body">
-    <div className="container">
 
-        <form className="form ">
-            <h1 className="form__header">
-                Kalkulator walut
-            </h1>
-            <fieldset className="form__fieldset">
-                
-                <p>
-                    <label>
-                        <span className="form__labelText">Złoty:</span>
-                        <input className="form__field " type="number" min="1" step="0.01" required />
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <span className="form__labelText">Waluta:</span>
-                        <select className="form__field">
-                            <option value="EUR">Euro</option>
-                            <option value="USD">Dolar amerykański</option>
-                            <option value="GBP">Funt brytyjski</option>
-                        </select>
-                    </label>
-                </p>
-            </fieldset>
-            <p>
-                <button className=" form__button">Oblicz kurs</button>
-            </p>
-            <p>
-                <button type="reset" className=" form__button">Wyczyść!</button>
-            </p>
-        </form>
-        <p className="text_result">Posiadasz: <strong className=" text_result">N/A</strong></p>
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+    .find(({ short }) => short === currency)
+    .rate;
+    
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+
+  return (
+    <div className="app">
+      <Form
+        result={result}
+        calculateResult={calculateResult}
+        />
     </div>
-</body>
   );
 }
 
 export default App;
+
+  
+
+
---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.36,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.64,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.41,
+    },
+];
