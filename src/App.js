8667915c3e7fcc8fb1f71ceb8cdce81de92aa028
Michał Styliński <stylinskimichal@gmail.com>
2024-03-06 16:13:13 +0100

    build new app and folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body className="body">
+    <div className="container">
+
+        <form className="form ">
+            <h1 className="form__header">
+                Kalkulator walut
+            </h1>
+            <fieldset className="form__fieldset">
+                
+                <p>
+                    <label>
+                        <span className="form__labelText">Złoty:</span>
+                        <input className="form__field " type="number" min="1" step="0.01" required />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">Waluta:</span>
+                        <select className="form__field">
+                            <option value="EUR">Euro</option>
+                            <option value="USD">Dolar amerykański</option>
+                            <option value="GBP">Funt brytyjski</option>
+                        </select>
+                    </label>
+                </p>
+            </fieldset>
+            <p>
+                <button className=" form__button">Oblicz kurs</button>
+            </p>
+            <p>
+                <button type="reset" className=" form__button">Wyczyść!</button>
+            </p>
+        </form>
+        <p className="text_result">Posiadasz: <strong className=" text_result">N/A</strong></p>
     </div>
+</body>
   );
 }
 
