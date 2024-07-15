0e213c41f41330d7149ddf7a096d67c765fda245
Michał Styliński <stylinskimichal@gmail.com>
2024-07-15 19:59:06 +0200

    add date on right corner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Form } from './Form';
+import {Clock} from "./Clock";
 import { currencies } from './currencies';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
 
   return (
     <div className="app">
+      <Clock/>
       <Form
         result={result}
         calculateResult={calculateResult}
